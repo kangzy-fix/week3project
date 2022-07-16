087b0ff94c3a1b9e755475bac71634fc3a59c0f7
kangzy-fix <kangzy.fix@student.moringaschool.com>
2022-07-16 17:09:29 +0300

    update

--- projo.js
+++ projo.js
@@ -1,29 +1,51 @@
 document.getElementById("submitButton").onclick = function(){
     let quantityInputA = document.getElementById("quantityInputA").value;
-    console.log(quantityInputA);
+    document.getElementById("shedA").innerHTML=quantityInputA;
+
 
     let quantityInputB = document.getElementById("quantityInputB").value;
-    console.log(quantityInputB);
+    document.getElementById("shedB").innerHTML=quantityInputB;
+
 
     let quantityInputC = document.getElementById("quantityInputC").value;
-    console.log(quantityInputC);
+    document.getElementById("shedC").innerHTML=quantityInputC;
+
 
     let quantityInputD = document.getElementById("quantityInputD").value;
-    console.log(quantityInputD);
+    document.getElementById("shedD").innerHTML=quantityInputD;
+
+    let totalProduction = 0;
+    let sumArray = [ quantityInputA,quantityInputB,quantityInputC, quantityInputD ];
+
+    for(var i=0;i<sumArray.length;i++){
+
+    console.log(sumArray[i]);
+    totalProduction += Number(sumArray[i]);
+    }
+
+    document.getElementById("totalP").innerHTML=totalProduction;
+
 }
-var totalProduction = document.getElementById("totalProduction").innerHTML = function(){
-    let quantityInputA = document.getElementById("quantityInputA").value;
-    // return quantityInputA;
 
-    let quantityInputB = document.getElementById("quantityInputB").value;
-    // return quantityInputB;
+function incomeOverTime(selling_price, time){
+    
 
-    let quantityInputC = document.getElementById("quantityInputC").value;
-    // return quantityInputC;
-
-    let quantityInputD = document.getElementById("quantityInputD").value;
-    // return quantityInputD;
-
-    totalProduction = quantityInputA + quantityInputB + quantityInputC + quantityInputD
-    console.log(totalProduction)
 }
+
+
+
+// var totalProduction = document.getElementById("totalProduction").innerHTML = function(){
+//     let quantityInputA = document.getElementById("quantityInputA").value;
+//     // return quantityInputA;
+
+//     let quantityInputB = document.getElementById("quantityInputB").value;
+//     // return quantityInputB;
+
+//     let quantityInputC = document.getElementById("quantityInputC").value;
+//     // return quantityInputC;
+
+//     let quantityInputD = document.getElementById("quantityInputD").value;
+//     // return quantityInputD;
+
+//     totalProduction = quantityInputA + quantityInputB + quantityInputC + quantityInputD
+//     console.log(totalProduction)
