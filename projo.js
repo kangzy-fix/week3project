6c84f2bc778d635deeddae55b22046ad559f0e24
kangzy-fix <kangzy.fix@student.moringaschool.com>
2022-08-19 18:55:33 +0300

    update

--- projo.js
+++ projo.js
@@ -54,3 +54,11 @@ function dailyProduction() {
 
 }
 submitButton.addEventListener("click", dailyProduction)
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
